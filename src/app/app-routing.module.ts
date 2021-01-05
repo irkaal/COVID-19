ea08bce921dc04c931b6e03b726621667a285170
irkaal <irkaalv@gmail.com>
2021-01-04 17:58:48 -0800

    Add map component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { MapComponent } from './map/map.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MapComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
