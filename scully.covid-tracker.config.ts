23ff91ed1a449e07536af234df9cf75f8f758e6c
irkaal <irkaalv@gmail.com>
2021-01-04 17:45:59 -0800

    init angular and scully project

---
+++ scully.covid-tracker.config.ts
@@ -0,0 +1,9 @@
+import { ScullyConfig } from '@scullyio/scully';
+
+export const config: ScullyConfig = {
+  projectRoot: './src',
+  projectName: 'covid-tracker',
+  outDir: './docs',
+  distFolder: './docs/dist',
+  routes: {},
+};
